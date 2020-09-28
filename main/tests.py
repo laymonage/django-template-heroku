0c9680380c03ec18aff56a67e6ebe72f47f3aba1
sage <laymonage@gmail.com>
2020-09-28 13:54:45 +0700

    main/tests: Create test for root url status

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class MainTestCase(TestCase):
+    def test_root_url_status_200(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
