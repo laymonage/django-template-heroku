f2076a9b0d2c39570c4bb721d8c7ba1482653fe3
sage <laymonage@gmail.com>
2020-09-28 17:20:05 +0700

    template: Convert project to template
    Use curly braces for project_name, so that it's automatically replaced
    with the project name on project generation.

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,4 +1,4 @@
-"""project_name URL Configuration
+"""{{ project_name }} URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
